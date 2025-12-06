120219acdd9a18d0e492ec4a6054bdb1d0600dc1
stew.chan@gmail.com <stew.chan@gmail.com>
2025-12-05 23:10:33 -0500

    Add swift-testing dependency for package tests

--- Package.swift
+++ Package.swift
@@ -5,7 +5,8 @@ let package = Package(
     name: "WristBop",
     platforms: [
         .watchOS(.v10),
-        .iOS(.v16)
+        .iOS(.v16),
+        .macOS(.v13)
     ],
     products: [
         .library(
@@ -13,13 +14,19 @@ let package = Package(
             targets: ["WristBopCore"]
         )
     ],
+    dependencies: [
+        .package(url: "https://github.com/swiftlang/swift-testing.git", from: "0.12.0")
+    ],
     targets: [
         .target(
             name: "WristBopCore"
         ),
         .testTarget(
             name: "WristBopCoreTests",
-            dependencies: ["WristBopCore"]
+            dependencies: [
+                "WristBopCore",
+                .product(name: "Testing", package: "swift-testing")
+            ]
         )
     ]
 )
